ea592d4f81f8f0f49a7307e565acd2f6659f2239
hgsk <hgsk85@gmail.com>
2014-03-19 13:05:00 +0900

    add project

---
+++ Assets/Standard Assets/Water (Basic)/Sources/Textures/batiscaf.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class batiscaf : MonoBehaviour {
+
+	public float speed = 5;
+	public float rotateSpeed = 5;
+	//barast range: +-600
+	public float barast = -100;
+	public float coefficient = 100;
+	//barast tank spec 1~2
+	public float tankspec = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody.AddForce(transform.forward * Input.GetAxisRaw("Vertical") * speed);
+		rigidbody.AddTorque(Vector3.up * Input.GetAxisRaw("Horizontal") * rotateSpeed);
+		barast += tankspec * Input.GetAxis("Flow");
+		Physics.gravity = new Vector3(0, barast/coefficient, 0);
+	}
+}
---
+++ Assets/Standard Assets/Water (Basic)/Sources/Textures/renderSettingController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class renderSettingController : MonoBehaviour {
+	public int deepdepth = 300;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//TODO better color 
+		RenderSettings.fogColor = new Color(0,0,(gameObject.transform.position.y-this.deepdepth)/this.deepdepth);
+	}
+}
